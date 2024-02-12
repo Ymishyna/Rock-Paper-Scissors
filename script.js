f9a477d4af7ab99b54e29f1eb57a11f698e177df
Yana Mishyna <yana.mishina.92@gmail.com>
2024-02-12 16:44:44 -0500

    cleaned the code

--- script.js
+++ script.js
@@ -4,17 +4,17 @@ const computerScoreSpan = document.querySelector('[data-computer-score]')
 const yourScoreSpan = document.querySelector('[data-your-score]')
 const SELECTIONS = [
     {
-        name:'rock',
+        name: 'rock',
         emoji: '✊',
         beats: 'scissors'
     },
     {
-        name:'paper',
+        name: 'paper',
         emoji: '✋',
         beats: 'rock'
     },
     {
-        name:'scissors',
+        name: 'scissors',
         emoji: '✌️',
         beats: 'paper'
     }
@@ -22,9 +22,9 @@ const SELECTIONS = [
 
 selectionButtons.forEach(selectionButton => {
     selectionButton.addEventListener('click', e => {
-       const selectionName = selectionButton.dataset.selection
-       const selection = SELECTIONS.find(selection => selection.name === selectionName)
-       makeSelection(selection)
+        const selectionName = selectionButton.dataset.selection
+        const selection = SELECTIONS.find(selection => selection.name === selectionName)
+        makeSelection(selection)
     })
 })
 
@@ -32,7 +32,6 @@ function makeSelection(selection) {
     const computerSelection = randomSelection()
     const yourWinner = isWinner(selection, computerSelection)
     const computerWinner = isWinner(computerSelection, selection)
-    // console.log(computerSelection)
 
     addSelectionResult(computerSelection, computerWinner)
     addSelectionResult(selection, yourWinner)
