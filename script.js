804f61c4f964b6f21af6f1187760ee9ef821a411
Yana Mishyna <yana.mishina.92@gmail.com>
2024-02-12 16:22:51 -0500

    trying to implement logic to results dinamicly

--- script.js
+++ script.js
@@ -1,12 +1,54 @@
 const selectionButtons = document.querySelectorAll("[data-selection]")
+const finalColum = document.querySelector("[data-final-colum]")
+const SELECTIONS = [
+    {
+        name:'rock',
+        emoji: '✊',
+        beats: 'scissors'
+    },
+    {
+        name:'paper',
+        emoji: '✋',
+        beats: 'rock'
+    },
+    {
+        name:'scissors',
+        emoji: '✌️',
+        beats: 'paper'
+    }
+]
 
-selectionButtons.forEach(selectionButtons => {
-    selectionButtons.addEventListener("click", e => {
-       const selectionName = selectionButtons.dataset.selection
-       makeSelection(selectionName)
+selectionButtons.forEach(selectionButton => {
+    selectionButton.addEventListener("click", e => {
+       const selectionName = selectionButton.dataset.selection
+       const selection = SELECTIONS.find(selection => selection.name === selectionName)
+       makeSelection(selection)
     })
 })
 
 function makeSelection(selection) {
-    console.log(selection)
+    const computerSelection = randomSelection()
+    const yourWinner = isWinner(selection, computerSelection)
+    const computerWinner = isWinner(computerSelection, selection)
+    // console.log(computerSelection)
+
+    addSelectionResult(computerSelection, computerWinner)
+    addSelectionResult(selection, yourWinner)
+}
+
+function addSelectionResult(selection, winner) {
+    const div = docyment.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add("result-selection")
+    if (winner) div.classList.add('winner')
+    finalColum.after(div)
+}
+
+function isWinner(selection, opponentSelection) {
+    return selection.beats === opponentSelection.name
+}
+
+function randomSelection() {
+    const randomIndex = Math.floor(Math.random() * SELECTIONS.length)
+    return SELECTIONS[randomIndex]
 }
